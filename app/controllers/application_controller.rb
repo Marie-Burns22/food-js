ccba864b44de9defd12972f3ce9d50acea6dd658
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-06-11 13:24:06 -0700

    Add helper methods to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,17 @@
 class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  helper_method :current_student
+  helper_method :logged_in?
+
+  private
+
+  def current_student
+    @current_student ||= Student.find(session[:student_id]) if session[:student_id]
+  end
+
+  def logged_in:
+    redirect_to '/' if !session[:student_id]
+  end 
+
 end
