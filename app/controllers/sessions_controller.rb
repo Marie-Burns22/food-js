44ec452d29d06b43af9d623b3af89aaa7c5b6fde
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-06-11 13:17:58 -0700

    Add create action to session controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,24 @@
-class ApplicationController < ActionController::Base
+class SesssionsController < ApplicationController
+
   def new
   end
 
   def create
-    session[:name] = params[:name]
-    redirect_to '/'
+    if params[:student][:name].nil? || params[:student][:name] == ""
+      redirect_to '/login'
+    else
+      student = Student.find_by(name: params[:student][:name])
+      if student
+        session[:student_id]= student.id
+        redirect_to "/foods"
+      else
+        redirect_to '/login'
+      end
+    end
   end
 
   def destroy
-    session.delete :name
+    session.delete :student_id if session[:student_id]
+    redirect_to "/"
   end
 end
