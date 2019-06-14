369a3b20bde471660d972f5b9326d7c4053d7180
Marie-Burns22 <41268371+Marie-Burns22@users.noreply.github.com>
2019-06-13 17:14:07 -0700

    Add units to unit select drop down menu

--- app/models/food.rb
+++ app/models/food.rb
@@ -9,15 +9,5 @@ class Food < ApplicationRecord
   def food_emissions
     self.emissions[0].amount if self.emissions[0]
   end
-#
-# #   def unit_g_amount
-#     self.emissions.amount where(self.emissions.unit: "g")
-#   end
 
 end
-
-# possible emission units
-# kg CO2e per lb of food
-# lbs of CO2e per serving
-# g of CO2e per serving
-# kg of CO2 per 50 g of protein
